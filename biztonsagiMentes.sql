df3ae8f175ae81410caa3dae61f0e2aef7953441
Brecsi <lastmans07@gmail.com>
2023-02-28 14:41:11 +0100

    SQL database

--- biztonsagiMentes.sql
+++ biztonsagiMentes.sql
@@ -1,45 +1,64 @@
--- database creation (adatbazis elkeszitese)
+﻿CREATE DATABASE autokolcsonzo
+CHARACTER SET utf8
+COLLATE utf8_hungarian_ci;
 
-CREATE DATABASE autoberles
-	CHARACTER SET utf8
-	COLLATE utf8_hungarian_ci;
 
-	CREATE TABLE autoberles.berlok (
-  id INT(11) NOT NULL AUTO_INCREMENT,
-  nev VARCHAR(100) NOT NULL,
-  jogositvany VARCHAR(15) NOT NULL,
-  telefonszam VARCHAR(20) DEFAULT NULL,
-  PRIMARY KEY (id)
-)
-ENGINE = INNODB;
-
-CREATE TABLE autoberles.autok (
-  id INT(11) NOT NULL AUTO_INCREMENT,
-  rendszam VARCHAR(6) NOT NULL,
-  tipus VARCHAR(100) NOT NULL,
-  evjarat INT(11) DEFAULT NULL,
-  szin VARCHAR(30) DEFAULT NULL,
+CREATE TABLE autokolcsonzo.cars (
+  id int(11) NOT NULL,
+  license varchar(6) NOT NULL,
+  type varchar(100) NOT NULL,
+  year int(11) DEFAULT NULL,
+  color varchar(30) DEFAULT NULL,
   PRIMARY KEY (id)
 )
 ENGINE = INNODB,
 CHARACTER SET utf8,
 COLLATE utf8_hungarian_ci;
 
-ALTER TABLE autoberles.autok 
-  ADD UNIQUE INDEX rendszam(rendszam);
+ALTER TABLE autokolcsonzo.cars
+ADD UNIQUE INDEX license (license);
 
-CREATE TABLE autoberles.kolcsonzes (
-  id INT(11) NOT NULL AUTO_INCREMENT,
-  berloid INT(11) DEFAULT NULL,
-  autoid INT(11) DEFAULT NULL,
-  berletkezdete DATETIME NOT NULL,
-  napokszama INT(11) DEFAULT NULL,
-  napidij INT(11) NOT NULL,
+CREATE TABLE autokolcsonzo.loaners (
+  id int(11) NOT NULL,
+  name varchar(100) NOT NULL,
+  licenseNum varchar(15) NOT NULL,
+  phoneNum varchar(15) DEFAULT NULL,
   PRIMARY KEY (id)
 )
 ENGINE = INNODB,
 CHARACTER SET utf8,
 COLLATE utf8_hungarian_ci;
 
+CREATE TABLE autokolcsonzo.users (
+  id int(11) NOT NULL,
+  firstName varchar(255) DEFAULT NULL,
+  lastName varchar(255) DEFAULT NULL,
+  gender varchar(255) DEFAULT NULL,
+  userName varchar(255) DEFAULT NULL,
+  email varchar(50) DEFAULT NULL,
+  password varchar(255) DEFAULT NULL,
+  number int(11) DEFAULT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8,
+COLLATE utf8_hungarian_ci;
 
+ALTER TABLE autokolcsonzo.users
+ADD UNIQUE INDEX userName (userName);
 
+ALTER TABLE autokolcsonzo.users
+ADD UNIQUE INDEX email (email);
+
+CREATE TABLE autokolcsonzo.loans (
+  id int(11) NOT NULL,
+  loanId int(11) DEFAULT NULL,
+  carId int(11) DEFAULT NULL,
+  loanStart datetime NOT NULL,
+  numberOfDays int(11) DEFAULT NULL,
+  dailyRate decimal(10, 0) NOT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8,
+COLLATE utf8_hungarian_ci;
