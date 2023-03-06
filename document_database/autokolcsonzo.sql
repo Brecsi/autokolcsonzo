cbf525eeecb12ae650e3e5907b028dff8b646587
Brecsi <lastmans07@gmail.com>
2023-03-06 09:19:48 +0100

    adatbázis kész

--- document_database/autokolcsonzo.sql
+++ document_database/autokolcsonzo.sql
@@ -2,3 +2,18 @@ select * from cars;
 select * from loaning;
 select * from renters;
 select * from users;
+
+
+-- 2003 után gyártott autók növekvõ sorrendben
+  select * from cars
+    where year >= 2003
+    order by year;
+
+-- 2023 után bérlõk neve
+  select renters.name, loanStart from loaning 
+    inner join renters on renters.id = renterId
+  where YEAR(loanStart) > 2022;
+
+-- 70-es telefonszámmal rendelkezõ bérlõk
+  select name, phone from renters
+  where phone HAVING '0670';
--- document_database/testData.sql
+++ document_database/testData.sql
@@ -2,12 +2,12 @@
 SELECT * FROM renters;
 SELECT * FROM loaning;
 SELECT * FROM users;
+DELETE FROM users;
 
-
+DELETE from loaning;
 DELETE FROM cars;
 DELETE FROM renters;
-DELETE FROM loans;
-DELETE FROM users;
+
 
 
 INSERT INTO cars (id, license, type, year, color)
@@ -24,7 +24,7 @@ INSERT INTO cars (id, license, type, year, color)
   (10, "SKY845", "Nissan", "2004", "Blue");
 
 
-INSERT INTO renters (id, name, licenseNum, phoneNum)
+INSERT INTO renters (id, name, license, phone)
   VALUES
   (1,  "Nagy Béla", "536012056314012", "06705519280"),
   (2,  "Kiss János", "509234527416861", "06306571231"),
@@ -39,11 +39,19 @@ INSERT INTO renters (id, name, licenseNum, phoneNum)
 
 
 
+
 INSERT INTO loaning (id, renterId, carId, loanStart, numDay, dailyRate)
   VALUES
-  (1, 1, 3, 20230105, 21, 25000),
-  (2, 4, 2, 20230210, NULL, 13000),
-  (3, 8, 1, 20221222, NULL, 17500),
-  (4, 5, 5, 20221210, 40, 11500),
-  (5, 2, 9, 20230101, NULL, 67000),
-  (6, 3, 6, 20221212, NULL, 12000);
+  (1,  1,  8, '2023.01.05', 21, 25000),
+  (2,  2,  7, '2023.02.10', NULL, 13000),
+  (3,  3,  6, '2022.12.22', NULL, 17500),
+  (4,  4,  5, '2022.12.10', 40, 11500),
+  (5,  5,  4, '2023.01.01', NULL, 67000),
+  (6,  6,  3, '2022.12.12', NULL, 12000),
+  (7,  7,  2, '2021.08.21', 70, 12000),
+  (8,  8,  1, '2022.10.11', 90, 15000),
+  (9,  9,  9, '2021.03.05', 44, 17000),
+  (10, 10,  10, '2022.11.20', 50, 11000);
+
+
+call autoberlesGen();
