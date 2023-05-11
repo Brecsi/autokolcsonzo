da134124ef6199ea7f81b702f5063a3376b72a6b
Brecsi <lastmans07@gmail.com>
2023-05-11 21:42:59 +0200

    axios install, register page start

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -4,87 +4,52 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-4 col-md-6 col-sm-8 w-auto mx-auto">
-            <div
-              v-if="!registerActive"
-              class="card login"
-              v-bind:class="{ error: emptyFields }"
-            >
+            <div v-if="!registerActive" class="card login card-body" v-bind:class="{ error: emptyFields }">
               <h1>Sign In</h1>
               <form class="form-group">
-                <input
-                  type="text"
-                  class="form-control"
-                  id="userName"
-                  v-model="storeLogin.userName"
-                  placeholder="Username"
-                  required
-                />
-                <input
-                  type="password"
-                  class="form-control"
-                  id="password"
-                  v-model="storeLogin.password"
-                  placeholder="Password"
-                  required
-                />
+                <input type="text" class="form-control" id="userName" v-model="storeLogin.userName" placeholder="Username"
+                  required />
+                <input type="password" class="form-control" id="password" v-model="storeLogin.password"
+                  placeholder="Password" required />
                 <button type="button" class="btn btn-primary" @click="login()">
                   Login
                 </button>
                 <p>
                   Don't have an account?
-                  <a
-                    href="#"
-                    @click="
-                      (registerActive = !registerActive), (emptyFields = false)
-                    "
-                    >Sign up here</a
-                  >
+                  <a href="#" @click="(registerActive = !registerActive), (emptyFields = false)
+                    ">Sign up here</a>
                 </p>
-                <div
-                  v-if="loginErrorMessage"
-                  class="alert alert-danger"
-                  role="alert"
-                >
+                <div v-if="loginErrorMessage" class="alert alert-danger" role="alert">
                   {{ loginErrorMessage }}
                 </div>
               </form>
             </div>
 
-            <div
-              v-else
-              class="card register"
-              v-bind:class="{ error: emptyFields }"
-            >
+            <div v-else class="card register card-body" v-bind:class="{ error: emptyFields }">
               <h1>Sign Up</h1>
               <form class="form-group">
-                <input
-                  type="text"
-                  class="form-control"
-                  id="userName"
-                  v-model="storeLogin.userName"
-                  placeholder="Username"
-                  required
-                />
-                <input
-                  type="password"
-                  class="form-control"
-                  id="password"
-                  v-model="storeLogin.password"
-                  placeholder="Password"
-                  required
-                />
-                <button class="btn btn-primary" @click="Register()">
+                <input type="text" class="form-control" id="firstName" v-model="storeLogin.firstName"
+                  placeholder="First Name" required />
+                <input type="text" class="form-control" id="lastName" v-model="storeLogin.lastName"
+                  placeholder="Last Name" required />
+                <input type="text" class="form-control" id="gender" v-model="storeLogin.gender" placeholder="Gender"
+                  required />
+                <input type="text" class="form-control" id="userName" v-model="storeLogin.userName" placeholder="Username"
+                  required />
+                <input type="email" class="form-control" id="email" v-model="storeLogin.email" placeholder="E-mail"
+                  required />
+                <input type="password" class="form-control" id="password" v-model="storeLogin.password"
+                  placeholder="Password" required />
+                <button class="btn btn-primary" @click="registerUser()">
                   Register
                 </button>
+                <div v-if="loginErrorMessage" class="alert alert-danger" role="alert">
+                  {{ loginErrorMessage }}
+                </div>
                 <p>
                   Already have an account?
-                  <a
-                    href="#"
-                    @click="
-                      (registerActive = !registerActive), (emptyFields = false)
-                    "
-                    >Sign in here</a
-                  >
+                  <a href="#" @click="(registerActive = !registerActive), (emptyFields = false)
+                    ">Sign in here</a>
                 </p>
               </form>
             </div>
@@ -100,16 +65,79 @@ import { useUrlStore } from "@/stores/url";
 const storeUrl = useUrlStore();
 import { useLoginStore } from "@/stores/login";
 import router from "../router";
+import axios from 'axios';
 const storeLogin = useLoginStore();
+
+class User {
+  constructor(
+    id = 0,
+    firstName = null,
+    lastNames = null,
+    gender = null,
+    userName = null,
+    email = null,
+    password = null
+  ) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastNames;
+    this.gender = gender;
+    this.userName = userName;
+    this.email = email;
+    this.password = password;
+  }
+}
+
 export default {
   data() {
     return {
       storeUrl,
       storeLogin,
+      users: [],
+      newUser: new User(),
       loginErrorMessage: null,
     };
   },
+  mounted() {
+    this.getUsers();
+  },
   methods: {
+
+    async getUsers() {
+      let url = this.storeUrl.urlUsers;
+      const config = {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${this.storeLogin.accessToken}`,
+        },
+      };
+      const response = await fetch(url, config);
+      const data = await response.json();
+      this.users = data.data;
+    },
+
+    async postUser() {
+      let url = this.storeUrl.urlUsers;
+      const body = JSON.stringify(this.newUser);
+      const config = {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${this.storeLogin.accessToken}`,
+        },
+        body: body,
+      };
+      const response = await fetch(url, config);
+      const data = await response.json();
+      console.log(data.data);
+      this.getUsers();
+    },
+
+    registerUser() {
+      this.newUser = new User();
+      this.postUser();
+    },
+
     loginErrorMessageShow(message) {
       this.loginErrorMessage = message;
       setTimeout(() => {
@@ -164,12 +192,21 @@ export default {
 </script>
 
 <style>
+div#app {
+  background-image: url(../../public/loginBackground.jpg);
+  background-attachment: fixed;
+  background-position: inherit;
+  background-repeat: no-repeat;
+  background-size: contain;
+}
+
 .my-width-login {
   max-width: 500px;
 }
 
 p {
   line-height: 1rem;
+  padding-top: 1rem;
 }
 
 .card {
@@ -179,10 +216,10 @@ p {
 .login-page {
   align-items: center;
   display: flex;
-  height: 500px;
+  height: 600px;
 }
 
 h1 {
-      margin-bottom: 1.5rem;
-   }
+  margin-bottom: 1.5rem;
+}
 </style>
